a6b25dfc6821cb034ce21113abfbb11578c59376
shao-shuai <shuai-shao@outlook.com>
2020-04-29 15:12:27 -0700

    add operator for creating s3 bucket and uploading files to s3

---
+++ src/airflow/dags/load_raw_dag.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta
+import os
+import configparser
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.postgres_operator import PostgresOperator
+from operators import CreateS3BucketOperator, UploadFileS3Operator
+
+default_args = {
+    'owner': 'Shuai',
+    'start_date': datetime(2019, 10, 25),
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': 300,
+    'email_on_retry': False
+}
+
+raw_datalake_bucket_name = 'shuaishao-udacity'
+
+dag = DAG('raw_datalake_dag',
+          default_args=default_args,
+          description='Load data into raw S3 datalake.',
+          schedule_interval='@monthly',
+          catchup=False
+          )
+
+start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
+
+create_raw_datalake = CreateS3BucketOperator(
+    task_id='Create_raw_datalake',
+    bucket_name=raw_datalake_bucket_name,
+    dag=dag
+)
+
+upload_trip_data = UploadFileS3Operator(
+	task_id='upload_trip_data',
+	bucket_name=raw_datalake_bucket_name,
+	path='/opt/bitnami/dataset',
+	dag=dag
+	)
+
+end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
+
+# define dependency
+start_operator >> create_raw_datalake
+create_raw_datalake >> upload_trip_data
+upload_trip_data >> end_operator
---
+++ src/airflow/plugins/operators/__init__.py
@@ -0,0 +1,8 @@
+from operators.create_s3_bucket import CreateS3BucketOperator
+from operators.upload_file_s3 import UploadFileS3Operator
+
+__all__ = [
+			'CreateS3BucketOperator',
+			'UploadFileS3Operator'
+
+]
